16924656179d44f217b577c9e59a8286fb757589
ismail harmanda <iharmanda@hotmail.com>
2021-07-31 18:55:16 +0300

    ListPage searchbar logo nd search input implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
+import ListPage from "./components/ListPage";
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <LandingPage />
-    </>
-  );
+  return <ListPage />;
 };
 
 export default App;
--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,7 +1,23 @@
 import React from "react";
+import SearchBar from "./facilities/SearchBar";
+import Button from "./facilities/Button";
+import Logo from "../img/logo.png";
+import "./ListPage.css";
 
 const ListPage = () => {
-  return <></>;
+  return (
+    <div className="row mt-5">
+      <div className="col-2">
+        <img className="smallLogo" alt="Logo" src={Logo}></img>
+      </div>
+      <form className="col-10">
+        <div className="row">
+          <SearchBar />
+          <Button text="Search" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ListPage;
