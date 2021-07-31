cfaa9fe2ff4c0540e2c16b9ec32c72c0809617d2
ismail harmanda <iharmanda@hotmail.com>
2021-07-31 18:19:17 +0300

    small text implemented under the logo

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,6 +7,9 @@ const LandingPage = () => {
   return (
     <div className="row justify-content-center">
       <img className="logo col-12" alt="Logo" src={Logo} />
+      <h6 className="col-xl-10 col-lg-9 col-md-8 col-6 offset-xl-2 offset-lg-3 offset-md-4 offset-6 text-center">
+        <small className="">Search web app</small>
+      </h6>
       <div className="col-12">
         <SearchForm />
       </div>
---
+++ src/components/ListPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListPage = () => {
+  return <></>;
+};
+
+export default ListPage;
