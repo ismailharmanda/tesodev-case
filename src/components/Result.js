e11513388fe7fd6e6edefcfd807456cda6dc31e4
ismail harmanda <iharmanda@hotmail.com>
2021-07-31 19:42:35 +0300

    create result component

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Result.css";
 
 const Result = () => {
-  return <div>Result</div>;
+  return (
+    <div className="row">
+      <div className="row offset-2">
+        <div className="col-6 text-left">Turkey - Ankara</div>
+        <div className="col-6 text-left">Email: abc@xyz.com</div>
+        <div className="col-12 text-left">
+          <small>Jane Doe - 2016</small>
+        </div>
+        <hr className="line" />
+      </div>
+    </div>
+  );
 };
 
 export default Result;
